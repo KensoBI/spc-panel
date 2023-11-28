eb726dcb1e9c98120f23d9ca610c72095235bc33
Grzegorz Nowak <gnowak@kensobi.com>
2023-11-28 18:16:58 +0100

    spcCalculations - tests

--- src/data/spcCalculations.test.ts
+++ src/data/spcCalculations.test.ts
@@ -1,4 +1,4 @@
-import { calcMin, calcMax, calcRange, calcMean, stdDev, calcLcl } from './spcCalculations';
+import { calcMin, calcMax, calcRange, calcMean, stdDev, calcLcl, calcValueSampleSize, calcUcl } from './spcCalculations';
 
 describe('calcMin', () => {
   it('should return the minimum value in the array', () => {
@@ -47,6 +47,7 @@ describe('calcLcl-range', () => {
     expect(calcLcl(field, 'range', 5)?.[0]).toBe(0.0);
     expect(calcLcl(field, 'range', 6)?.[0]).toBe(0.0);
     expect(calcLcl(field, 'range', 7)?.[0]).toBe(0.304);
+    expect(calcLcl(field, 'range', 8)?.[0]).toBe(0.544);
   });
 });
 
@@ -63,3 +64,49 @@ describe('calcLcl-mean', () => {
     expect(calcLcl(field, 'standardDeviation', 2)?.[0]).toBe(4.620235708272902);
   });
 });
+describe('calcUcl-range', () => {
+  it('should return upper control limit of the array (range aggregation)', () => {
+    const field = [1, 2, 3, 4, 5];
+    expect(calcUcl(field, 'range', 2)?.[0]).toBe(13.068)
+    expect(calcUcl(field, 'range', 3)?.[0]).toBe(10.296)
+    expect(calcUcl(field, 'range', 4)?.[0]).toBe(9.128)
+  })
+})
+
+describe('calcUcl-mean', () => {
+  it('should return the upper control limit of the array (mean aggregation)', () => {
+    const field = [1, 2, 3, 4, 5];
+    expect(calcLcl(field, 'mean', 2)).toStrictEqual([-4.52, -0.7603938623500599]);
+    expect(calcLcl(field, 'mean', 7)).toStrictEqual([1.324, 1.3283995692750017]);
+  });
+});
+
+describe('calcUcl-stdDev', () => {
+  it('should return the upper control limit of the array (standardDeviation aggregation)', () => {
+    const field = [1, 2, 3, 4, 5];
+    expect(calcLcl(field, 'standardDeviation', 2)?.[0]).toBe(4.620235708272902);
+    expect(calcLcl(field, 'standardDeviation', 8)?.[0]).toBe(2.5667976157071677);
+  });
+});
+
+describe('calcValueSampleSize-range',() => {
+  it('should return a new vector with aggregated values (range aggregation)', () => {
+    const field = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    expect(calcValueSampleSize(field, 2, 'range')).toStrictEqual([0, 1, 1, 1, 1])
+    expect(calcValueSampleSize(field, 5, 'range')).toStrictEqual([3, 4])
+  })
+})
+describe('calcValueSampleSize-mean',() => {
+  it('should return a new vector with aggregated values (mean aggregation)', () => {
+    const field = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    expect(calcValueSampleSize(field, 2, 'mean')).toStrictEqual([1, 2.5, 4.5, 6.5, 8.5])
+    expect(calcValueSampleSize(field, 6, 'mean')).toStrictEqual([2, 6.5])
+  })
+})
+describe('calcValueSampleSize-stdDev',() => {
+  it('should return a new vector with aggregated values (standardDeviation aggregation)', () => {
+    const field = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    expect(calcValueSampleSize(field, 2, 'standardDeviation')).toStrictEqual([0, 0.5, 0.5, 0.5, 0.5])
+    expect(calcValueSampleSize(field, 7, 'standardDeviation')).toStrictEqual([0.5, 2])
+  })
+})
