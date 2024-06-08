25e9196b383502563ecc6bdcab90bcf1d226b0fc
Tomasz Czerkas <tomeqc@gmail.com>
2024-06-07 23:03:16 -0400

    Revert "fixed merge issue"
    This reverts commit 96cbc1fe8f2d1c7244ada2ef76cc666359b1a815.

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { PanelPlugin } from '@grafana/data';
+import { FieldConfigProperty, PanelPlugin } from '@grafana/data';
 import { PanelOptions, defaultPanelOptions } from './types';
 import { ChartPanel } from './components/ChartPanel';
 import { LimitsEditor } from './components/options/LimitsEditor';
@@ -8,54 +8,66 @@ import { SpcOptionEditor } from 'components/options/SpcOptionEditor';
 import { parseData } from 'data/parseData';
 import { ChartPresets } from 'components/options/ChartPresets';
 
-export const plugin = new PanelPlugin<PanelOptions>(ChartPanel).setPanelOptions((builder) => {
-  builder.addCustomEditor({
-    id: 'spcOptions',
-    path: 'spcOptions',
-    name: 'Chart preset',
-    description: 'Select chart type to get sample settings.',
-    defaultValue: defaultPanelOptions.spcOptions,
-    editor: ChartPresets,
-    category: ['Chart'],
-    showIf: (_, data) => parseData(data ?? []).hasTableData === false,
-  });
-  builder.addCustomEditor({
-    id: 'spcOptions',
-    path: 'spcOptions',
-    name: 'SPC options',
-    description: 'Select options for SPC chart. You can enter a custom sample size value by typing a number.',
-    defaultValue: defaultPanelOptions.spcOptions,
-    editor: SpcOptionEditor,
-    category: ['Chart'],
-    showIf: (_, data) => parseData(data ?? []).hasTableData === false,
-  });
-  builder.addCustomEditor({
-    id: 'spcOptions.constantsConfig',
-    path: 'spcOptions.constantsConfig',
-    name: 'Constants',
-    description: 'Add constants for the chart',
-    defaultValue: defaultPanelOptions.spcOptions?.constantsConfig,
-    editor: ConstantsListEditor,
-    category: ['Chart'],
-  });
-  builder.addCustomEditor({
-    id: 'limitConfig',
-    path: 'limitConfig',
-    name: 'Limits',
-    description: 'Upper and lower limits for the chart',
-    defaultValue: defaultPanelOptions.limitConfig,
-    editor: LimitsEditor,
-    category: ['Chart'],
-  });
-  builder.addCustomEditor({
-    id: 'timeseriesParams',
-    path: 'timeseriesParams',
-    name: 'Options',
-    description: 'Timeseries settings',
-    defaultValue: defaultPanelOptions.timeseriesParams,
-    editor: SimpleParamsEditor,
-    category: ['Chart'],
-  });
+export const plugin = new PanelPlugin<PanelOptions>(ChartPanel)
+  .useFieldConfig({
+    disableStandardOptions: [
+      FieldConfigProperty.Unit,
+      FieldConfigProperty.NoValue,
+      FieldConfigProperty.Thresholds,
+      FieldConfigProperty.Mappings,
+      FieldConfigProperty.Links,
+      FieldConfigProperty.Color,
+      FieldConfigProperty.Filterable,
+    ],
+  })
+  .setPanelOptions((builder) => {
+    builder.addCustomEditor({
+      id: 'spcOptions',
+      path: 'spcOptions',
+      name: 'Chart preset',
+      description: 'Select chart type to get sample settings.',
+      defaultValue: defaultPanelOptions.spcOptions,
+      editor: ChartPresets,
+      category: ['Chart'],
+      showIf: (_, data) => parseData(data ?? []).hasTableData === false,
+    });
+    builder.addCustomEditor({
+      id: 'spcOptions',
+      path: 'spcOptions',
+      name: 'SPC options',
+      description: 'Select options for SPC chart. You can enter a custom sample size value by typing a number.',
+      defaultValue: defaultPanelOptions.spcOptions,
+      editor: SpcOptionEditor,
+      category: ['Chart'],
+      showIf: (_, data) => parseData(data ?? []).hasTableData === false,
+    });
+    builder.addCustomEditor({
+      id: 'spcOptions.constantsConfig',
+      path: 'spcOptions.constantsConfig',
+      name: 'Constants',
+      description: 'Add constants for the chart',
+      defaultValue: defaultPanelOptions.spcOptions?.constantsConfig,
+      editor: ConstantsListEditor,
+      category: ['Chart'],
+    });
+    builder.addCustomEditor({
+      id: 'limitConfig',
+      path: 'limitConfig',
+      name: 'Limits',
+      description: 'Upper and lower limits for the chart',
+      defaultValue: defaultPanelOptions.limitConfig,
+      editor: LimitsEditor,
+      category: ['Chart'],
+    });
+    builder.addCustomEditor({
+      id: 'timeseriesParams',
+      path: 'timeseriesParams',
+      name: 'Options',
+      description: 'Timeseries settings',
+      defaultValue: defaultPanelOptions.timeseriesParams,
+      editor: SimpleParamsEditor,
+      category: ['Chart'],
+    });
 
-  return builder;
-});
+    return builder;
+  });
